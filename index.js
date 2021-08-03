77ce97adb842ee55a3b527cc475d4c32ab1f0576
Daniel Velazquez <daniel.velazquez.sallago.contractor@bbva.com>
2021-08-03 11:38:34 -0500

    probando sistema de arranque

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get('/',(req,res)=>{
      </html>`)
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
   console.log('Servidor inciado...');
